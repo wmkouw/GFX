0a1e3c62b2dc619582d95162c990f640e52e8052
wmkouw <wmkouw@gmail.com>
2021-09-10 23:32:07 +0200

    Small bug fix

--- src/gfx_update_rules.jl
+++ src/gfx_update_rules.jl
@@ -29,7 +29,7 @@ function ruleVariationalGFXOutNPPPPP(Δt :: Float64,
 	# Set order of system
 	order = dims(marg_θ)
 
-	# Define helper matrices
+	# Structure matrices
 	S = Bidiagonal(ones(order,), Δt.*ones(order-1,), :U)
 	s = zeros(order,); s[end] = Δt
 
@@ -62,7 +62,7 @@ function ruleVariationalGFXIn1PNPPPP(Δt :: Float64,
 	# Set order of system
 	order = dims(marg_x)
 
-	# Define helper matrices
+	# Structure matrices
 	S = Bidiagonal(ones(order,), Δt.*ones(order-1,), :U)
 	s = zeros(order,); s[end] = Δt
 
@@ -103,7 +103,7 @@ function ruleVariationalGFXIn2PPNPPP(Δt :: Float64,
 	# Set order of system
 	order = dims(marg_θ)
 
-	# Define helper matrices
+	# Structure matrices
 	S = Bidiagonal(ones(order,), Δt.*ones(order-1,), :U)
 	s = zeros(order,); s[end] = Δt
 
@@ -141,7 +141,7 @@ function ruleVariationalGFXIn3PPPNPP(Δt :: Float64,
 	# Set order
 	order = dims(marg_θ)
 
-	# Define helper matrices
+	# Structure matrices
 	S = Bidiagonal(ones(order,), Δt.*ones(order-1,), :U)
 	s = zeros(order,); s[end] = Δt
 
@@ -189,9 +189,9 @@ function ruleVariationalGFXIn5PPPPPN(Δt :: Float64,
 	# Set order
 	order = dims(marg_θ)
 
-	# Define helper matrices
+	# Structure matrices
 	S = Bidiagonal(ones(order,), Δt.*ones(order-1,), :U)
-	s = zeros(order,); s[end] = -Δt
+	s = zeros(order,); s[end] = Δt
 	
 	# Compute expected values
 	EA = S + s*mθ'
